6cd1bef526489fa95fe2620a7186a86906b2cd95
sowmiyasuba <31585889+sowmiyasuba@users.noreply.github.com>
2018-01-19 16:48:05 +0530

    Create amstronginterval.c

---
+++ amstronginterval.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+int n1,n2,num,temp,rem;
+printf(" enter The two intervals:");
+scanf("%d%d",&n1&n2);
+printf("amstrong num b/w %d and %d are:",n1,n2);
+for(i=n1+1;i<n2;i++)
+{
+temp=i;
+num=0;
+while(temp!=0)
+{
+rem=(temp%10);
+num+=rem*rem*rem;
+temp/=10;
+}
+if(i==num)
+{
+printf("%d",i);
+}
+}
+}
+
