f0edf04445469f0822e96914bc13916f48fb8e7f
sowmiyasuba <31585889+sowmiyasuba@users.noreply.github.com>
2018-01-19 16:33:20 +0530

    Create amstrong.c

---
+++ amstrong.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int num,rem=0,cub=0;sum=0,temp;
+printf("enter the num:");
+scanf("%d",&n);
+while(num!=0)
+{
+rem=num%10;
+cub=pow(rem,3);
+sum=sum+cube;num=num/10;
+if(num==temp)
+
+printf("The num is amstrong",num);
+else
+printf("The num is not amstrong",num);
+}
+}
