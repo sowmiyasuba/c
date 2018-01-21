7031dbc5fc8b86dd246f5b46ae9681e80b7bde05
sowmiyasuba <31585889+sowmiyasuba@users.noreply.github.com>
2018-01-21 10:04:04 +0530

    Create multiple.c

---
+++ multiple.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+clrscr();
+int i,d,n;
+printf("Enter the limit:");
+scanf("%d",d);
+printf("Enter the num:");
+scanf(%d",n);
+pintf("The multiples of given %d is :\n",n);
+for(i=0;i<=n;i++)
+if(i/n==0)
+printf("%d",&i);
+getch();
+}
