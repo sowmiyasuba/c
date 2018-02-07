3965c16e98d13e0895219b92cb2ef046ab7eba6e
sowmiyasuba <31585889+sowmiyasuba@users.noreply.github.com>
2018-02-07 16:57:42 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1,n2,sum=0;
+    
+    printf("enter two num:");
+    scanf("%d%d",&n1,&n2);
+    sum=n1+n2;
+   
+    printf("the sum of two no is:%d",sum);
+    
+  return 0;
+}
